b9da6554241cbb9f8b082ad151d607b8d407879e
williamdrade2026-Dot <williamdrade2026@gmail.com>
2026-07-06 21:30:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const { Client, GatewayIntentBits, Events } = require("discord.js");
+const http = require("http");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds],
 });
 
 client.once(Events.ClientReady, () => {
-  console.log("Bot online!");
+  console.log(`Bot online!`);
 });
 
 client.login(process.env.TOKEN);
+
+// Servidor HTTP para o Render
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Bot online!");
+}).listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
